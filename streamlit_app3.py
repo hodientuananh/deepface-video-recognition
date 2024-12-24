complete fix query 1 image and get average weight

streamlit_app3.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -59,7 +60,7 @@ CHOSEN_PARAMS[MOVIE.FOLDER] = MOVIES_DIR_MAPPING[CHOSEN_PARAMS[MOVIE.NAME]]
 CHOSEN_PARAMS[MOVIE.CHARACTER] = st.sidebar.selectbox('Choose character', option_characters , label_visibility="collapsed")
 
 st.sidebar.markdown("<h1 style='font-size: 16px;'>Want to compare between Face Detection - Model Embedding</h1>", unsafe_allow_html=True)
-compare_bw_face_and_em = st.sidebar.checkbox("Yes", value=False)
+compare_bw_face_and_em = st.sidebar.checkbox("Yes", value=True)
 
 if not compare_bw_face_and_em:
     st.sidebar.markdown("<h1 style='font-size: 16px;'>Choose face detection type</h1>", unsafe_allow_html=True)
@@ -82,11 +83,14 @@ for file in lst_images:
     image = image.resize((120,120))
     Images.append(image)
 for idx, Image in enumerate(Images):
-    str_key = "chk_{}".format(idx)
+    str_key = f'chk_{idx}'
     with columns_imgs[idx]:
         st.image(Image)
-        st.checkbox(lst_images[idx].split('.')[0], key=str_key, value=False)
-        
+        st.checkbox(lst_images[idx].split('.')[0], key=str_key, value=False, on_change=lambda idx=idx: uncheck_others(idx))
+def uncheck_others(selected_idx):
+    for i in range(len(lst_images)):
+        if i != selected_idx:
+            st.session_state[f'chk_{i}'] = False
 # ###################################################################################################
 # INIT GLOBAL PARAMS
 ## Initialize global parameters
@@ -98,29 +102,31 @@ path.set_topK(topK)
 path.set_evaluation_file(topK)
 
 shot = Shot(path)
+shot.init_shot_result()
+
 # ###################################################################################################
 # RETRIEVE THE TOP K RESULTS
 ## Show result of specific detection and embedding model
 if compare_bw_face_and_em == False:
     path.set_global_path(CHOSEN_PARAMS[DETECTION.MODEL], CHOSEN_PARAMS[EMBBEDDING.FOLDER]) 
-    shot.init_shot_result()
+    shot.init_face_det_model_emb()
     for character_image_index in range(len(lst_images)):
         chk_key, character_key = get_check_key_and_character_key(character_image_index)
         if chk_key in st.session_state and st.session_state[chk_key]:   
             path.set_chosen_avatar_emb_path(character_image_index, lst_images)
-            character_result_dict = shot.get_shots_per_character()
+            character_result_dict = shot.get_sorted_shots_per_character()
             shot.add_to_shot_result(character_key, character_result_dict)
 ## Show result of all detection and embedding model
 else:
-    for character_image_index in range(len(lst_images)):
-        chk_key, character_key = get_check_key_and_character_key(character_image_index)   
-        for (face_det, emb_fol) in DET_EMB_MAPPING:
-            path.set_global_path(face_det, emb_fol)
-            shot.init_shot_result()
+    for (face_det, emb_fol) in DET_EMB_MAPPING:
+        path.set_global_path(face_det, emb_fol)
+        shot.init_face_det_model_emb()
+        for character_image_index in range(len(lst_images)):
+            chk_key, character_key = get_check_key_and_character_key(character_image_index) 
             if chk_key in st.session_state and st.session_state[chk_key]:
                 path.set_chosen_avatar_emb_path(character_image_index, lst_images)
-                character_result_set = shot.get_shots_per_character()
-                shot.add_to_shot_result(character_key, character_result_set)
+                character_result_dict = shot.get_sorted_shots_per_character()
+                shot.add_to_shot_result(character_key, character_result_dict)
 
 # # ######################################################################################################
 # EVALUATION METRICS
@@ -131,17 +137,11 @@ df_ground_truth_full = df["Full"]
 lst_groundtruth = [i for i in df_ground_truth_full]
 evaluation_file = path.get_evaluation_file()
 
-st.write(shot.result)
-
 if not compare_bw_face_and_em:
     # face_det_model_emb_name = get_face_det_model_emb_name()
-    dict_predict = shot.get_sorted_weight_dict_per_character()
-    st.write("dict_predict", dict_predict)
-    lst_predict = get_topK_first_elements(dict_predict, topK)
-    st.write("lst_predict", lst_predict)
+    lst_predict = shot.get_result_per_face_det_model_emb(topK=topK)
     evaluation = Evaluation(lst_predict, lst_groundtruth)
     precision_scores, recall_scores, f1_score, ap = evaluation.calculate_metrics()
-    st.write('sum(precision_scores)', sum(precision_scores), 'len(lst_groundtruth)', len(lst_groundtruth))
     chart_data = pd.DataFrame(
         {
             "Precsion": convert_list_to_numpy_array(precision_scores),
@@ -155,8 +155,7 @@ if not compare_bw_face_and_em:
 else:
     for (face_det, emb_fol) in DET_EMB_MAPPING:
         path.set_global_path(face_det, emb_fol)
-        dict_predict = shot.get_sorted_weight_dict_per_character()
-        lst_predict = get_topK_first_elements(dict_predict, topK)
+        lst_predict = shot.get_result_per_face_det_model_emb(topK=topK)
         evaluation = Evaluation(lst_predict, lst_groundtruth)
         precision_scores, recall_scores, f1_score, ap = evaluation.calculate_metrics()
         rows = np.append(rows, ap)
@@ -168,15 +167,18 @@ else:
             "Average Precision": rows,
         }
     )
+    # Sort the DataFrame by "Average Precision" in descending order
+    sorted_chart_data = chart_data.sort_values(by="Average Precision", ascending=False)
+
     st.bar_chart(
-        chart_data,
+        sorted_chart_data,
         x="Face Detection and Model Embedding",
         y="Average Precision",
     )
-    st.table(chart_data)
-    chart_data.to_csv(evaluation_file, sep=',', encoding='utf-8', index=False, header=True)
+    st.table(sorted_chart_data)
+    sorted_chart_data.to_csv(evaluation_file, sep=',', encoding='utf-8', index=False, header=True)
 
-# ######################################################################################################
+# # ######################################################################################################
 # SHOW THE VIDEO RESULTS
 num_batches = ceil(len(lst_predict)/BATCH_SIZE)
 